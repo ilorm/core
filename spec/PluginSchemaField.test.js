469a0bd6d25049f9dd28292ac0ca43cc25a6889b
stombre <guillaumedaix@free.fr>
2019-01-13 18:00:59 +0100

    FIX core by writing new test

---
+++ spec/PluginSchemaField.test.js
@@ -0,0 +1,44 @@
+const { expect, } = require('chai');
+
+// Create a clean instance of ilorm :
+const Ilorm = require('..').constructor;
+const ilorm = new Ilorm();
+const { Schema } = ilorm;
+
+describe('spec ilorm - Plugin SchemaField', () => {
+  describe('Check Schema Field casting feature', () => {
+    it('1 - Plugin can declare if it listen specific field value in SchemaField', () => {
+      Schema.declarePluginOption('pluginField');
+
+      const schema = new Schema({});
+
+      expect(schema.pluginField.length).to.be.equal(0);
+    });
+
+    it('2 - SchemaField with specific attribute field need will be identified', () => {
+      const trackedField = Schema.boolean();
+      trackedField.pluginField = true;
+
+      const schema = new Schema({
+        tracked: trackedField,
+        notTracked: Schema.boolean(),
+      });
+
+      expect(schema.getFieldsAssociatedWithPlugin('pluginField')).to.be.deep.equal([
+        'tracked',
+      ]);
+    });
+
+    it('3 - SchemaField with unknown attribute field need will not be identified', () => {
+      const trackedField = Schema.boolean();
+      trackedField.anotherField = true;
+
+      const schema = new Schema({
+        tracked: trackedField,
+        notTracked: Schema.boolean(),
+      });
+
+      expect(schema.getFieldsAssociatedWithPlugin('anotherField')).to.be.deep.equal([]);
+    });
+  });
+});
---
+++ spec/SchemaDefaultInit.test.js
@@ -0,0 +1,61 @@
+const { expect, } = require('chai');
+
+// Create a clean instance of ilorm :
+const Ilorm = require('..').constructor;
+const { Schema } = new Ilorm();
+
+describe('spec ilorm', () => {
+  describe('Schema default init', () => {
+    it('Without default, the init will do nothing', async () => {
+      const schema = new Schema({
+        firstName: Schema.string(),
+        age: Schema.number(),
+      });
+
+      const obj = await schema.init();
+
+      expect(obj).to.deep.equal({});
+    });
+
+    it('With default declared, the object will be init with default value if nothing is set', async () => {
+      const schema = new Schema({
+        firstName: Schema.string().default('guillaume'),
+        age: Schema.number(),
+      });
+
+      const obj = await schema.init();
+
+      expect(obj).to.deep.equal({
+        firstName: 'guillaume',
+      });
+    });
+
+    it('Default do not erase parameter (if something is set)', async () => {
+      const schema = new Schema({
+        firstName: Schema.string().default('guillaume'),
+        age: Schema.number(),
+      });
+
+      const obj = await schema.init({
+        firstName: 'John'
+      });
+
+      expect(obj).to.deep.equal({
+        firstName: 'John',
+      });
+    });
+
+    it('Default function will be called', async () => {
+      const schema = new Schema({
+        firstName: Schema.string(),
+        age: Schema.number().default(() => 10),
+      });
+
+      const obj = await schema.init();
+
+      expect(obj).to.deep.equal({
+        age: 10,
+      });
+    });
+  });
+});
