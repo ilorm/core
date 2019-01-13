f27048d273a133c0bc4101cfc9ca7729e2d89c35
stombre <guillaumedaix@free.fr>
2019-01-13 17:14:07 +0100

    ADD requiredField test and fix behavior of isValid

---
+++ spec/RequiredField.test.js
@@ -0,0 +1,31 @@
+const { expect, } = require('chai');
+
+// Create a clean instance of ilorm :
+const Ilorm = require('..').constructor;
+const ilorm = new Ilorm();
+
+describe('spec ilorm - RequiredField', () => {
+  describe('Required field', () => {
+    it('Undefined required field are not valid', async () => {
+      const boolean = new ilorm.Schema.Types.Boolean;
+
+      boolean.required();
+
+      expect(await boolean.isValid()).to.be.equal(false);
+    });
+
+    it('Undefined unrequired field are valid', async () => {
+      const boolean = new ilorm.Schema.Types.Boolean;
+
+      boolean.required(false);
+
+      expect(await boolean.isValid()).to.be.equal(true);
+    });
+
+    it('Per default field are unrequired', async () => {
+      const boolean = new ilorm.Schema.Types.Boolean;
+
+      expect(await boolean.isValid()).to.be.equal(true);
+    });
+  });
+});
