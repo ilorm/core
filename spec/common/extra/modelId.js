acae792aef64325a9b8a3a684b26da3d4ac70e47
stombretrooper <evirtuel@gmail.com>
2020-09-26 17:19:57 +0200

    FIX test (using fixtures inside testContext)

--- spec/common/extra/modelId.js
+++ spec/common/extra/modelId.js
@@ -12,9 +12,9 @@ module.exports = (TestContext) => {
     it('Should instantiate id from direct id or by field', () => {
       const Customers = testContext.Models.customers;
 
-      const CUSTOMER_ID_A = Customers.id(testContext.ID.GUILLAUME);
+      const CUSTOMER_ID_A = Customers.id(testContext.fixtures.ID.GUILLAUME);
       const CUSTOMER_ID_B = Customers.id({
-        [testContext.idFieldName]: testContext.ID.GUILLAUME,
+        [testContext.idFieldName]: testContext.fixtures.ID.GUILLAUME,
       });
 
       expect(CUSTOMER_ID_A).to.deep.equal(CUSTOMER_ID_B);
@@ -23,11 +23,11 @@ module.exports = (TestContext) => {
     it('Should resolve instance from id', async () => {
       const Customers = testContext.Models.customers;
 
-      const CUSTOMER_ID = Customers.id(testContext.ID.GUILLAUME);
+      const CUSTOMER_ID = Customers.id(testContext.fixtures.ID.GUILLAUME);
 
       const customer = await CUSTOMER_ID.resolveInstance();
 
-      expect(customer).to.deep.equal(testContext.getCustomersFixture().GUILLAUME);
+      expect(customer).to.deep.equal(testContext.fixtures.getCustomersFixture().GUILLAUME);
     });
   });
 };
