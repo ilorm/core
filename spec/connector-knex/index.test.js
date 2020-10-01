d8a546d525b3368cb1e7c5b1a1bc84ec8c2c24ee
stombretrooper <evirtuel@gmail.com>
2020-10-01 23:59:01 +0200

    FIX run all tests

--- spec/connector-knex/index.test.js
+++ spec/connector-knex/index.test.js
@@ -2,9 +2,5 @@ const TestContext = require('ilorm-connector-knex/spec/testContext.class');
 
 const testCommon = require('../common');
 
-testCommon(TestContext, {
-  query: {
-    linkedWith: true,
-  },
-});
+testCommon(TestContext);
 
