4fa0641337c86c34d4af753a54a6bce5bad2e97f
stombretrooper <evirtuel@gmail.com>
2020-10-04 13:22:14 +0200

    ADD test on Model.save (case of insert)

--- spec/common/base/model.js
+++ spec/common/base/model.js
@@ -1,6 +1,6 @@
 const { expect, } = require('chai');
 
-const FAKE_DATE = 3232323232;
+const FAKE_DATE = new Date('2020-10-01');
 const dateNow = Date.now;
 
 module.exports = (TestContext) => {
@@ -30,6 +30,33 @@ module.exports = (TestContext) => {
         paidAt: null,
         isPaid: false,
         amount: null,
+      });
+    });
+
+    it('Should insert model into database', async () => {
+      Date.now = () => FAKE_DATE;
+
+      const Invoices = testContext.Models.invoices;
+
+      const invoiceToSave = new Invoices();
+
+      invoiceToSave.id = 'INSERT_ID';
+      invoiceToSave.customerId = testContext.fixtures.ID.CUSTOMERS.BENJAMIN;
+      invoiceToSave.amount = 300;
+
+      await invoiceToSave.save();
+
+      const invoice = await Invoices.query()
+        .id.is('INSERT_ID')
+        .findOne();
+
+      expect(invoice).to.deep.equal({
+        id: 'INSERT_ID',
+        customerId: testContext.fixtures.ID.CUSTOMERS.BENJAMIN,
+        createdAt: FAKE_DATE,
+        paidAt: null,
+        isPaid: false,
+        amount: 300,
       });
     });
   });
