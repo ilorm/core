b24e87b00b71703b77e5f67182a8bdc5ce92e6b3
stombretrooper <evirtuel@gmail.com>
2020-10-27 23:28:02 +0100

    ADD test on casting null and undefined scenario

--- spec/SchemaFieldCast.test.js
+++ spec/SchemaFieldCast.test.js
@@ -10,12 +10,16 @@ describe('spec ilorm', () => {
       const boolean = new ilorm.Schema.Types.Boolean();
 
       expect(boolean.castValue('true')).to.be.a('boolean');
+      expect(boolean.castValue(undefined)).to.be.equal(undefined);
+      expect(boolean.castValue(null)).to.be.equal(null);
     });
 
     it('Should cast a date value', () => {
       const date = new ilorm.Schema.Types.Date();
 
       expect(date.castValue('11/11/2018')).to.be.a('date');
+      expect(date.castValue(undefined)).to.be.equal(undefined);
+      expect(date.castValue(null)).to.be.equal(null);
     });
 
     it('Should not create a new date instance when casting', () => {
@@ -23,18 +27,24 @@ describe('spec ilorm', () => {
       const rawDate = new Date('11/11/2018');
 
       expect(date.castValue(rawDate)).to.be.equal(rawDate);
+      expect(date.castValue(undefined)).to.be.equal(undefined);
+      expect(date.castValue(null)).to.be.equal(null);
     });
 
     it('Should cast a number value', () => {
       const number = new ilorm.Schema.Types.Number();
 
       expect(number.castValue('33')).to.be.a('number');
+      expect(number.castValue(undefined)).to.be.equal(undefined);
+      expect(number.castValue(null)).to.be.equal(null);
     });
 
     it('Should cast a string value', () => {
       const string = new ilorm.Schema.Types.String();
 
       expect(string.castValue(0)).to.be.a('string');
+      expect(string.castValue(undefined)).to.be.equal(undefined);
+      expect(string.castValue(null)).to.be.equal(null);
     });
   });
 });
